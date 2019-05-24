dabb4ae0f5bf88750439448ee2f2ef2a6b90d779
Guillaume Becker <guillaume@plutusds.com>
2019-05-24 19:00:28 +0900

    feature add localisation base and presentational container separation logic

---
+++ locale/home/ko.js
@@ -0,0 +1,5 @@
+export default {
+	home: {
+		content: '홈 페이지에 오신 것을 환영합니다.'
+	}
+};
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,56 @@
 import axios from 'axios';
 import BigNumber from 'bignumber.js';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
+import en from 'react-intl/locale-data/en';
+import ko from 'react-intl/locale-data/ko';
+import { addLocaleData, IntlProvider } from 'react-intl';
+import flattenMessages from '../utils/i18n';
+import locale from '../locale/home';
 
 import Layout from '../components/common/Layout';
 import Todo from '../components/Todo';
+import MainTitle from '../containers/MainTitle';
 
-// Port in to using useState hooks, if you need state
-const Index = ({ stars }) => (
-	<Layout>
-		<div name={`next-repo-stars-${stars}`}>
-			<h3 style={{ textAlign: 'center' }}>Welcome to the Home page</h3>
-		</div>
-		<Todo />
-	</Layout>
-);
+addLocaleData([...en, ...ko]);
+
+class Index extends Component {
+	static async getInitialProps() {
+		const res = await axios.get('https://api.github.com/repos/zeit/next.js');
+		const json = await res.data;
+
+		const nextStars = new BigNumber(json.stargazers_count);
+		return { stars: nextStars.toString() };
+		// return '0';
+	}
+	// Port in to using useState hooks, if you need state
+	render() {
+		const { stars } = this.props;
+		const currentLanguage = 'ko';
+
+		return (
+			<Layout>
+				<IntlProvider
+					locale={currentLanguage}
+					key={currentLanguage}
+					messages={flattenMessages(locale[currentLanguage])}
+				>
+					<div>
+						<div name={`next-repo-stars-${stars}`}>
+							<MainTitle />
+						</div>
+						<Todo />
+					</div>
+				</IntlProvider>
+			</Layout>
+		);
+	}
+}
 
 Index.propTypes = {
 	stars: PropTypes.string.isRequired
-};
-
-Index.getInitialProps = async () => {
-	// Adding a default/initialState can be done as follows:
-
-	const res = await axios.get('https://api.github.com/repos/zeit/next.js');
-	const json = await res.data;
-
-	const nextStars = new BigNumber(json.stargazers_count);
-	return { stars: nextStars.toString() };
 };
 
 export default connect()(Index);
