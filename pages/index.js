ffcc438e2a12eedc8c41121afd9c22b87d95ed88
Guillaume Becker <guillaume+github@plutusds.com>
2020-07-06 12:08:22 +0900

    remove unused code in the index main page.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-import axios from 'axios';
-import BigNumber from 'bignumber.js';
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import { IntlProvider } from 'react-intl';
@@ -12,42 +9,23 @@ import Layout from 'src/components/common/Layout';
 import Todo from 'src/components/Todo';
 import MainTitle from 'src/components/MainTitle';
 
-class Index extends Component {
-  static async getInitialProps() {
-    const res = await axios.get('https://api.github.com/repos/zeit/next.js');
-    const json = await res.data;
+const Index = () => {
+  const currentLanguage = 'en';
 
-    const nextStars = new BigNumber(json.stargazers_count);
-    return { stars: nextStars.toString() };
-    // return '0';
-  }
-
-  // Port in to using useState hooks, if you need state
-  render() {
-    const { stars } = this.props;
-    const currentLanguage = 'ko';
-
-    return (
-      <Layout>
-        <IntlProvider
-          locale={currentLanguage}
-          key={currentLanguage}
-          messages={flattenMessages(locale[currentLanguage])}
-        >
-          <div>
-            <div name={`next-repo-stars-${stars}`}>
-              <MainTitle />
-            </div>
-            <Todo />
-          </div>
-        </IntlProvider>
-      </Layout>
-    );
-  }
-}
-
-Index.propTypes = {
-  stars: PropTypes.string.isRequired,
+  return (
+    <Layout>
+      <IntlProvider
+        locale={currentLanguage}
+        key={currentLanguage}
+        messages={flattenMessages(locale[currentLanguage])}
+      >
+        <div>
+          <MainTitle />
+          <Todo />
+        </div>
+      </IntlProvider>
+    </Layout>
+  );
 };
 
 export default connect()(Index);
