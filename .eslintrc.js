df8239f87a0a78fc59d9842a944b3174129d648c
Guillaume <guillaume@getawair.com>
2021-10-18 14:42:40 +0900

    Update dependencies, remove aws-amplify since unused and will not use, change babel parser pkg to @babel/eslint-parser use, add add .dccache file to gitignore (generated by Snyk VScode extension)

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     'prettier',
   ],
   globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' },
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaFeatures: { jsx: true },
     ecmaVersion: 2018,
