0e96f4d7d98fa26dbfd4caa497ca0fe063a945c0
wakie92 <wakie9292@gmail.com>
2021-12-07 02:04:06 +0900

    Add load to firestore config:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,13 +8,13 @@ import ReduxToastr from 'react-redux-toastr';
 import PropTypes from 'prop-types';
 import { Provider, useStore } from 'react-redux';
 // To enable Firebase/Firestore config needs add a valid Firebase app keys to .env.local
-// import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
-// import { createFirestoreInstance } from 'redux-firestore';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
 import { ReactQueryDevtools } from 'react-query-devtools';
 
-import constants from 'src/utils/constants';
+import { ErrorLevels } from 'src/utils/constants';
 import ErrorBoundary from 'src/components/shared/ErrorBoundary';
-// import firebaseApp from 'src/utils/firebaseApp';
+import firebaseApp from 'src/utils/firebaseApp';
 import ThemeHandler from 'src/components/shared/ThemeHandler';
 import LocaleContextProvider from 'src/contexts/LocaleContext';
 import UiUxContextProvider from 'src/contexts/UiUxContext';
@@ -28,10 +28,10 @@ import { GlobalStyles } from 'src/styles/styledComponents/globalStyled';
 import 'rsuite/dist/styles/rsuite-default.css';
 import 'src/styles/index.scss';
 
-// const rrfConfig = {
-//   userProfile: null,
-//   // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
-// };
+const rrfConfig = {
+  userProfile: null,
+  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+};
 
 // Top Progress bar
 NProgress.configure();
@@ -42,15 +42,15 @@ Router.events.on('routeChangeError', () => NProgress.done());
 const App = ({ Component, pageProps }) => {
   const store = useStore();
 
-  // const rrfProps = {
-  //   firebase: firebaseApp,
-  //   config: rrfConfig,
-  //   dispatch: store.dispatch,
-  //   createFirestoreInstance,
-  // };
+  const rrfProps = {
+    firebase: firebaseApp,
+    config: rrfConfig,
+    dispatch: store.dispatch,
+    createFirestoreInstance,
+  };
 
   return (
-    <ErrorBoundary errorLevel={constants.ERRORS_LEVELS.application}>
+    <ErrorBoundary errorLevel={ErrorLevels.application}>
       <Head>
         {/* Meta */}
         <meta charSet="utf-8" />
@@ -61,26 +61,26 @@ const App = ({ Component, pageProps }) => {
         />
       </Head>
       <Provider store={store}>
-        {/* <ReactReduxFirebaseProvider {...rrfProps}> */}
-        <RoutingContextProvider>
-          <UiUxContextProvider>
-            <ThemeHandler>
-              <LocaleContextProvider>
-                <ReduxToastr
-                  preventDuplicates
-                  position="top-right"
-                  transitionIn="bounceInDown"
-                  transitionOut="bounceOutUp"
-                  closeOnToastrClick
-                  progressBar
-                />
-                <GlobalStyles />
-                <Component {...pageProps} />
-              </LocaleContextProvider>
-            </ThemeHandler>
-          </UiUxContextProvider>
-        </RoutingContextProvider>
-        {/* </ReactReduxFirebaseProvider> */}
+        <ReactReduxFirebaseProvider {...rrfProps}>
+          <RoutingContextProvider>
+            <UiUxContextProvider>
+              <ThemeHandler>
+                <LocaleContextProvider>
+                  <ReduxToastr
+                    preventDuplicates
+                    position="top-right"
+                    transitionIn="bounceInDown"
+                    transitionOut="bounceOutUp"
+                    closeOnToastrClick
+                    progressBar
+                  />
+                  <GlobalStyles />
+                  <Component {...pageProps} />
+                </LocaleContextProvider>
+              </ThemeHandler>
+            </UiUxContextProvider>
+          </RoutingContextProvider>
+        </ReactReduxFirebaseProvider>
         {process.env.NEXT_PUBLIC_APP_ENV !== 'production' && (
           <ReactQueryDevtools initialIsOpen={false} position="bottom-left" />
         )}
